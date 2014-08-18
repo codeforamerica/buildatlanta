24c2d8d857ba1173540d70aaa54553485d8ac8d4
Sam Hashemi <sam@samhashemi.com>
2014-08-18 16:48:14 -0400

    Make the random points fall within the NPU, for demo purposes.

--- app/models/project.js
+++ app/models/project.js
@@ -2,6 +2,10 @@ var Model = require('ampersand-model');
 var config = require('../config.js');
 var accounting = require('accounting');
 
+var polygons = require('../npupolygons');
+var inside = require('point-in-polygon');
+
+
 module.exports = Model.extend({
   props: {
     id: 'any',
@@ -33,6 +37,23 @@ module.exports = Model.extend({
       if (resp['Neighborhood ' + i]) neighborhoods.push(resp['Neighborhood ' + i]);
     }
 
+    // Generate up to 100 points to get it inside the correct NPU polygon
+    // Just for demo purposes until we get the real data.
+    // TODO: REMOVE THIS WHEN REAL DATA IS PROVIDED
+    var lat, lng, mainNPU = polygons[resp['NPU 1']];
+
+    if (mainNPU) {
+      for (var j = 0; j < 1000; j++) {
+        lat = mainNPU[0][1] + (Math.random() - 0.5) * 0.1;
+        lng = mainNPU[0][0] + (Math.random() - 0.5) * 0.1;
+        if (inside([lng, lat], mainNPU)) break;
+      }
+    } else {
+      lat = config.map.center[0];
+      lng = config.map.center[1];
+    }
+
+
     return {
       name: resp['Project Name'],
       description: resp['Project Description'],
@@ -41,8 +62,7 @@ module.exports = Model.extend({
       category: resp['Category'],
       npus: npus,
       neighborhoods: neighborhoods,
-      latlng: [config.map.center[0] + (Math.random() - 0.5) * 0.2,
-               config.map.center[1] + (Math.random() - 0.5) * 0.2],
+      latlng: [lat, lng],
     };
   }
 });
