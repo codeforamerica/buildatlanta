642193c10580a30605203bbe6e1bcded787ef88f
Sam Hashemi <sam@samhashemi.com>
2014-09-07 10:40:05 -0700

    Modify language.

--- app/views/main.js
+++ app/views/main.js
@@ -28,7 +28,7 @@ module.exports = View.extend({
       </header>
 
       <div class="content">
-        <div class="context">The City of Atlanta expects to invest $300 million dollars to build a better city. Explore the map to see how the money is being spent and share your thoughts.</div>
+        <div class="context">The City of Atlanta is investing $250 million dollars to build a better city. Explore the map to see potential projects and programs and share your thoughts.</div>
         <div class="links">
           <a href="http://www.atlantaga.gov/infrastructure" target="_blank">Read more</a> • 
           <a href="http://www.atlantaga.gov/modules/showdocument.aspx?documentid=6033" target="_blank">Commenting Policy</a> • 
