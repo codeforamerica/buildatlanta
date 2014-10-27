68aac4a760025c973f0490ef0c541d3d2b7690dd
Sam Hashemi <sam@samhashemi.com>
2014-10-27 11:46:19 -0400

    Grammar.

--- app/views/thankyou.js
+++ app/views/thankyou.js
@@ -6,7 +6,7 @@ module.exports = View.extend({
       <div class="comments">
         <h1>Thank You!</h1>
         <p class="thankyou">Thank you for your participation! Thousands of people visited and left over 400 comments.</p>
-        <p>Thanks so far, we’re looking at these now, but keep the comments coming! You can also email us at <a class="linklike" href="mailto:infrastructure@atlantaga.gov">infrastructure@atlantaga.gov</a>. Stay tuned for more on the proposed infrastructure bond!</p>
+        <p class="thankyou">We’re looking at these now, but keep the comments coming! You can also email us at <a class="linklike" href="mailto:infrastructure@atlantaga.gov">infrastructure@atlantaga.gov</a>. Stay tuned for more on the proposed infrastructure bond!</p>
         <p class="thankyou"><span class="linklike">Continue to the site</span> to see all the proposed projects.</p>
       </div>
     </div>
